5d3b5c6e75718d549696788070b3af0f2875f833
inhzus <inhzus@gmail.com>
2020-03-21 21:44:10 +0800

    [Feat] simpler func traits instantiation

--- src/func_traits/main.cc
+++ src/func_traits/main.cc
@@ -6,29 +6,25 @@
 
 #include <functional>
 
-template <typename T> struct remove_func_cv { using type = T; };
+template <typename T> struct remove_func_class { using type = T; };
 template <typename R, typename C, typename... Args>
-struct remove_func_cv<R (C::*)(Args...) const>
-    : public remove_func_cv<R (C::*)(Args...)> {};
+struct remove_func_class<R (C::*)(Args...)>
+    : public remove_func_class<R (*)(Args...)> {};
 template <typename R, typename C, typename... Args>
-struct remove_func_cv<R (C::*)(Args...) volatile>
-    : public remove_func_cv<R (C::*)(Args...)> {};
+struct remove_func_class<R (C::*)(Args...) const>
+    : public remove_func_class<R (*)(Args...)> {};
 template <typename R, typename C, typename... Args>
-struct remove_func_cv<R (C::*)(Args...) const volatile>
-    : public remove_func_cv<R (C::*)(Args...)> {};
+struct remove_func_class<R (C::*)(Args...) volatile>
+    : public remove_func_class<R (*)(Args...)> {};
+template <typename R, typename C, typename... Args>
+struct remove_func_class<R (C::*)(Args...) const volatile>
+    : public remove_func_class<R (*)(Args...)> {};
 
-template <typename T> using remove_func_cv_t = typename remove_func_cv<T>::type;
+template <typename T>
+using remove_func_class_t = typename remove_func_class<T>::type;
 
 template <typename T, typename = void>
-struct func_traits : func_traits<remove_func_cv_t<T>> {};
-template <typename R, typename C, typename... Args>
-struct func_traits<R (C::*)(Args...)> { // void (foo::Bar::*)(int, float)
-  using result_type = R;
-  using args_type = std::tuple<Args...>;
-  template <size_t idx>
-  using arg_type_at = std::tuple_element_t<idx, args_type>;
-  static constexpr size_t arg_count = sizeof...(Args);
-};
+struct func_traits : func_traits<remove_func_class_t<T>> {};
 template <typename R, typename... Args> struct func_traits<R (*)(Args...)> {
   using result_type = R;
   using args_type = std::tuple<Args...>;
@@ -65,3 +61,4 @@ int main() {
   func_traits<decltype(&go_temp<int>)> d;
   return 0;
 }
+
